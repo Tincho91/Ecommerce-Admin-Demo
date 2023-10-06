5f5cb931c10141bdbaaa401dcb839426d512072c
Martin Maiaru <tincho_91_15@hotmail.com>
2023-10-06 00:36:51 -0300

    Commit 1- DEMO

---
+++ app/(dashboard)/[storeId]/(routes)/sizes/components/columns.tsx
@@ -0,0 +1,32 @@
+"use client"
+
+import { ColumnDef } from "@tanstack/react-table"
+
+import { CellAction } from "./cell-action"
+
+export type SizeColumn = {
+  id: string
+  name: string;
+  value: string;
+  createdAt: string;
+}
+
+export const columns: ColumnDef<SizeColumn>[] = [
+  {
+    accessorKey: "name",
+    header: "Nombre",
+  },
+  {
+    accessorKey: "value",
+    header: "Valor (s, m, l)",
+  },
+  {
+    accessorKey: "createdAt",
+    header: "Fecha",
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => <CellAction data={row.original} />,
+    header: "Acciones",
+  },
+];
